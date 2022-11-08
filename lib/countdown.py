1c69121fd900c1c5c32f8cc21b06b9f5f57c7dfa
Sam <samuel.waters@gmail.com>
2022-11-08 15:14:36 -0700

    works two ways but fails tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
-# your code goes here!
+# def countdown(num):
+#     '''counts down from number and prints "HAPPY NEW YEAR!"'''
+#     for i in range(num, 0, -1):
+#         print(f"{i} SECOND(S)!")
+#     print("HAPPY NEW YEAR!")
+
+def countdown(num):
+    i = num
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+    
+
+
+countdown(10)
